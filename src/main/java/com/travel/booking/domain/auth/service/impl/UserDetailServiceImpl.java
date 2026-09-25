17a3994ba11a3383ae88f76c84fb7a1d31c6773c
Adrianus Simarmata <adrianussimarmata9@gmail.com>
2026-09-25 10:39:08 +0700

    fix(sonarqube): resolve maintainability issues

--- src/main/java/com/travel/booking/domain/auth/service/impl/UserDetailServiceImpl.java
+++ src/main/java/com/travel/booking/domain/auth/service/impl/UserDetailServiceImpl.java
@@ -8,13 +8,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
 
